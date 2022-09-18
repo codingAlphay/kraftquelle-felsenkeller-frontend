2ed0fbd5526ff50eae35be8c1ba6e727a3c6eb8e
Alpay Aksu <2110653914@stud.fh-kufstein.ac.at>
2022-09-18 02:53:32 +0200

    Update animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,13 +16,19 @@ module.exports = {
         'sans': ['KoHo', ...defaultTheme.fontFamily.sans],
       },
       animation: {
-        fade: 'fadeIn 3s ease-in-out',
-        wiggle: 'wiggle 1s ease-in-out infinite'
+        fade: 'fadeIn 2s ease-in-out',
+        fastfade: 'fadeIn 1s ease-in-out',
+        wiggle: 'wiggle 1s ease-in-out infinite',
+        slideup: 'slideup .5s ease-in-out'
       },
       keyframes: {
         fadeIn: {
           '0%': { opacity: 0 },
           '100%': { opacity: 1 },
+        },
+        slideup: {
+          '0%': { bottom:-50 },
+          '100%': { bottom:0 },
         },
         wiggle: {
           '0%, 100%': { transform: 'rotate(-3deg)' },
